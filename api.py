d253d2b5d01bc08d25933f4c91209e0914b1a5af
w3sm1l3 <cool.makowka@ya.ru>
2023-12-19 22:19:02 +0700

    v0.8 new api

--- api.py
+++ api.py
@@ -1,26 +1,61 @@
 import json
-import requests
+from dataclasses import dataclass, asdict
 from datetime import datetime
 
-#with open('data.txt') as json_file:
-    #data = json.load(json_file)
+adict = {"0": "A", "1": "B", "2": "C", "3": "D", "4": "E", "5": "F", "6": "G", "7": "H", "8": "I", "9": "J"}
 
-#ndata = json.dumps(data, indent=4)
-#for film in data['docs']:
-#    print(film['id'])
-#    print(film['name'])
-#    print(film['genres'])
 
-#print(data['docs'][0]['id'])
+@dataclass
+class FilmInfo:
+    fId: int
+    title: str
+    rateKp: float
+    rateImdb: float
+    bio: str
+    year: int
+    posterUrl: str
 
-# data['docs'][0]['id']
-# NAME data['docs'][0]['name']
-#
 
-def make_query(limit, sortField, yearFrom, yearTo, rateFrom, rateTo, genre_plus, genre_minus):
+def serialize_url(url):
+    class UrlReference:
+        def __init__(self, url):
+            self.url = url
+
+        def __str__(self):
+            return f"url({self.url})"
+
+    return json.dumps(UrlReference(url).__str__(), separators=(',', ':'))
+
+
+def intersect_dicts(dict1, dict2):
+    return {k: dict1[k] for k in dict1 if k in dict2 and dict1[k] == dict2[k]}
+
+
+def port_to_code(port):
+    port = str(port)
+    res = ""
+    for number in port:
+        res += adict[number]
+    return res + "\n"
+
+
+def make_query(limit, sortField, genre_plus, yearFrom="default", yearTo="default", rateFrom="default", rateTo="default", genre_minus="default"):
     url = "https://api.kinopoisk.dev/v1.4/movie?"
-    api_token = "CV8TT3N-4T547VH-HEWCBQP-TRRHEBE"
-    api_header = {"X-API-KEY": api_token}
+
+    if not genre_minus:
+        genre_minus = "default"
+
+    if not yearFrom:
+        yearFrom = "default"
+
+    if not yearTo:
+        yearTo = "default"
+
+    if not rateFrom:
+        rateFrom = "default"
+
+    if not rateTo:
+        rateTo = "default"
 
     limit_query = f"limit={limit}"
 
@@ -42,15 +77,21 @@ def make_query(limit, sortField, yearFrom, yearTo, rateFrom, rateTo, genre_plus,
         NewYearTo = datetime.now().year
         year_query = f"year={yearFrom}-{NewYearTo}"
     elif yearTo != "default" and yearFrom == "default":
-        year_query = f"year=1000-{yearTo}"
+        year_query = f"year=1900-{yearTo}"
     elif yearTo == "default" and yearFrom == "default":
         NewyearTo = datetime.now().year
-        year_query = f"year={yearFrom}-{NewyearTo}"
+        year_query = f"year=1900-{NewyearTo}"
     elif yearTo != "default" and yearFrom != "default":
-        year_query = f"year={yearFrom}-{yearTo}"
+        if yearTo == yearFrom:
+            year_query = f"year={yearFrom}"
+        else:
+            year_query = f"year={yearFrom}-{yearTo}"
 
     if rateFrom != "default" and rateTo != "default":
-        rate_query = f"rating.kp={rateFrom}-{rateTo}"
+        if rateFrom == rateTo:
+            rate_query = f"rating.kp={rateTo}"
+        else:
+            rate_query = f"rating.kp={rateFrom}-{rateTo}"
     if rateFrom == "default" and rateTo == "default":
         rate_query = f"rating.kp=5-10"
     if rateFrom != "default" and rateTo == "default":
@@ -60,18 +101,18 @@ def make_query(limit, sortField, yearFrom, yearTo, rateFrom, rateTo, genre_plus,
 
     if genre_minus == "default":
         genre_query = ""
-        if genre_plus.len() == 1:
-            genre_query = f"&genres.name={genre_plus}"
+        if len(genre_plus) == 1:
+            genre_query = f"&genres.name={genre_plus[0]}"
 
         else:
             for genre in genre_plus:
                 genre_query += f"&genres.name=+{genre}"
 
     else:
-        if genre_plus.len() == 1:
+        if len(genre_plus) == 1:
             genre_query = f"&genres.name=+{genre_plus}"
             for genre in genre_minus:
-                genre_query += f"&genres.name=+{genre}"
+                genre_query += f"&genres.name=!{genre}"
 
         else:
             genre_query = ""
@@ -80,25 +121,30 @@ def make_query(limit, sortField, yearFrom, yearTo, rateFrom, rateTo, genre_plus,
             for genre in genre_minus:
                 genre_query += f"&genres.name=!{genre}"
 
-    query = "{0}{1}{2}{3}&{4}&{5}&{6}&{7}&{8}&{9}".format(url, limit_query, select_query, null_query, sort_query,
+    query = "{0}{1}{2}{3}&{4}&{5}&{6}&{7}&{8}{9}".format(url, limit_query, select_query, null_query, sort_query,
                                                           sort_type_query,
                                                           status_query, year_query, rate_query, genre_query)
-    print(query)
+
+    return query
+
+
+def extract_data(data):
+    films = []
+    for film in data['docs']:
+        films.append(FilmInfo(film['id'], film['name'], film['rating']['kp'], film['rating']['imdb'], film['description'].rstrip('\r\n'), film['year'], film['poster']['url']))
+
+    return films
+
+
+def encode_data(data):
+    film_jsons = []
+    for film in data:
+        film_jsons.append(json.dumps(asdict(film)))
+
+    return film_jsons
 
 
 
 
 
-
-
-
-
-
-
-
-#request = requests.get(query, headers=api_header)
-#
-#print(request)
-#print(request.json())
-#print(request.text)
 
--- main.py
+++ main.py
@@ -1,62 +1,7 @@
 import socket
-import json
 import requests
 from _thread import *
-from dataclasses import dataclass, asdict
-
-adict = {"0": "A", "1": "B", "2": "C", "3": "D", "4": "E", "5": "F", "6": "G", "7": "H", "8": "I", "9": "J"}
-film_dict = {"триллер": "1",
-             "драма": "2",
-             "криминал": "3",
-             "мелодрама": "4",
-             "детектив": "5",
-             "фантастика": "6",
-             "приключения": "7",
-             "биография": "8",
-             "фильм-нуар": "9",
-             "вестерн": "10",
-             "боевик": "11",
-             "фэнтези": "12",
-             "комедия": "13",
-             "военный": "14",
-             "история": "15",
-             "музыка": "16",
-             "ужасы": "17",
-             "мультфильм": "18",
-             "семейный": "19",
-             "мюзикл": "20",
-             "спорт": "21",
-             "документальный": "22",
-             "короткометражка": "23",
-             "аниме": "24"}
-
-
-@dataclass
-class film_info:
-    id: int
-    name: str
-    rate: float
-    ratev2: float
-    year: int
-    poster_url: int
-
-    def info(self):
-        print(self.id, self.name, self.rate, self.ratev2, self.year, self.poster_url)
-
-
-def serialize_url(url):
-    class UrlReference:
-        def __init__(self, url):
-            self.url = url
-
-        def __str__(self):
-            return f"url({self.url})"
-
-    return json.dumps(UrlReference(url).__str__(), separators=(',', ':'))
-
-
-def intersect_dicts(dict1, dict2):
-    return {k: dict1[k] for k in dict1 if k in dict2 and dict1[k] == dict2[k]}
+from api import *
 
 
 class FilmusServer:
@@ -66,12 +11,11 @@ class FilmusServer:
         self.host = "192.168.0.12"
         self.port = 50000
         self.thread_count = 0
-        self.api_url = "https://kinopoiskapiunofficial.tech/api/v2.2/films?"
-        self.api_token = "6a0de868-081f-4100-b6a2-d8aa65dd3296"
+        self.api_token = "CV8TT3N-4T547VH-HEWCBQP-TRRHEBE"
         self.api_header = {"X-API-KEY": self.api_token}
-        self.api_req_data = []
         self.first_data = []
         self.second_data = []
+        self.solo_data = []
         self.result = []
         self.res_film_jsons = []
         self.clients = []
@@ -82,12 +26,12 @@ class FilmusServer:
     def clear_cache(self):
         self.films.clear()
         self.film_jsons.clear()
-        self.api_req_data.clear()
         self.first_data.clear()
         self.second_data.clear()
         self.result.clear()
         self.res_film_jsons.clear()
         self.clients.clear()
+        self.solo_data.clear()
         self.ready = 0
 
     def client_handler(self, connection):
@@ -95,6 +39,7 @@ class FilmusServer:
             flag = data.decode()
 
             if flag == "start":
+                # ЧИСТИМ КЭШ
                 self.clear_cache()
                 # ОТПРАВЛЯЕМ КОД ДЛЯ ПРИСОЕДИНЕНИЯ ВТОРОГО ЧЕЛОВЕКА (НА ОСНОВЕ ПОРТА)
                 code = port_to_code(self.port)
@@ -102,35 +47,52 @@ class FilmusServer:
 
                 # ПРИНИМАЕМ ПАРАМЕТРЫ ДЛЯ API ЗАПРОСА
                 data = connection.recv(2048)
-                self.api_req_data = json.loads(data[2:].decode('utf-8'))
-                print(self.api_req_data)
+                q_info = json.loads(data[2:].decode('utf-8'))
+                print(q_info)
 
-                # ПОЛУЧАЕМ СПИСОК ФИЛЬМОВ ИЗ API
-                target_url = "{0}&genres={1}&order={2}&type=FILM&page=1".format(self.api_url,
-                                                                                film_dict[self.api_req_data['genre']],
-                                                                                str(self.api_req_data['sortType']))
-                request = requests.get(target_url, headers=self.api_header)
+                # СОСТАВЛЯЕМ API ЗАПРОС
+                query = make_query(q_info['limit'], q_info['sortField'], q_info['genrePlus'], q_info['yearFrom'], q_info['yearTo'],
+                                   q_info['rateFrom'], q_info['rateTo'], q_info['genreMinus'])
 
-                # КОНВЕРТИРУЕМ JSON В ЭКЗЕМПЛЯРЫ ДАТАКЛАССА ДЛЯ КАЖДОГО ФИЛЬМА
-                for film in request.json()['items']:
-                    self.films.append(film_info(film['kinopoiskId'], film['nameRu'], film['ratingKinopoisk'],
-                                                film['ratingImdb'], film['year'], int(film['posterUrl'][54:-4])))
+                print(query)
 
-                # СОБИРАЕМ JSON МАССИВ ДЛЯ ОТПРАВКИ КЛИЕНТУ
-                for film in self.films:
-                    self.film_jsons.append(json.dumps(asdict(film)))
+                # ДЕЛАЕМ API ЗАПРОС
+                info = self.make_request(query)
+                # КОМПОНУЕМ ДАННЫЕ ИЗ API
+                self.films = extract_data(info)
+                # КОДИРУЕМ ДАННЫЕ ДЛЯ ОТПРАВКИ
+                self.film_jsons = encode_data(self.films)
+                # ОТПРАВЛЯЕМ ДАННЫЕ
+                self.send_data(connection)
+                connection.close()
 
-                # ОТПРАВЛЯЕМ JSON'Ы КЛИЕНТУ
-                for film in self.film_jsons:
-                    print("data sended")
-                    connection.sendall(film.encode())
+            elif flag == "startSolo":
+                # ЧИСТИМ КЭШ
+                self.clear_cache()
 
+                # ПРИНИМАЕМ ПАРАМЕТРЫ ДЛЯ API ЗАПРОСА
+                data = connection.recv(2048)
+                q_info = json.loads(data[2:].decode('utf-8'))
+
+                # СОСТАВЛЯЕМ API ЗАПРОС
+                query = make_query(q_info['limit'], q_info['sortField'], q_info['genrePlus'], q_info['yearFrom'], q_info['yearTo'],
+                                   q_info['rateFrom'], q_info['rateTo'], q_info['genreMinus'])
+
+                print(query)
+                ## ДЕЛАЕМ API ЗАПРОС
+                info = self.make_request(query)
+                ## КОМПОНУЕМ ДАННЫЕ ИЗ API
+                self.films = extract_data(info)
+                print(info)
+                ## КОДИРУЕМ ДАННЫЕ ДЛЯ ОТПРАВКИ
+                self.film_jsons = encode_data(self.films)
+                ## ОТПРАВЛЯЕМ ДАННЫЕ
+                self.send_data(connection)
                 connection.close()
 
             elif flag == "join":
-                for film in self.film_jsons:
-                    print(film)
-                    connection.sendall(film.encode())
+                print("second device connected")
+                self.send_data(connection)
                 connection.close()
 
             elif flag == "ready":
@@ -153,6 +115,10 @@ class FilmusServer:
                     print("second data recieved")
                     print(self.second_data)
 
+            elif flag == "resultSolo":
+                data = connection.recv(2048)
+                self.solo_data = json.loads(data[2:].decode('utf-8'))
+
             elif flag == "final":
                 self.clients.append(connection)
 
@@ -163,8 +129,8 @@ class FilmusServer:
 
                     for film in self.films:
                         for key in final.keys():
-                            if str(film.id) == str(key):
-                                print(f"id={film.id}, key={key}")
+                            if str(film.fId) == str(key):
+                                print(f"id={film.fId}, key={key}")
                                 self.result.append(film)
 
                     for film in self.result:
@@ -176,6 +142,24 @@ class FilmusServer:
                             conn.sendall(film.encode())
 
                         conn.close()
+
+            elif flag == "finalSolo":
+                solo = {key: val for key, val in self.solo_data.items() if val == "Right"}
+
+                for film in self.films:
+                    for key in solo.keys():
+                        if str(film.fId) == str(key):
+                            print(f"id={film.fId}, key={key}")
+                            self.result.append(film)
+
+                for film in self.result:
+                    self.res_film_jsons.append(json.dumps(asdict(film)))
+
+                for film in self.res_film_jsons:
+                    print(f"data send to {connection}")
+                    connection.sendall(film.encode())
+
+                connection.close()
 
     def accept_connections(self, server_socket):
         client, address = server_socket.accept()
@@ -194,13 +178,14 @@ class FilmusServer:
         while True:
             self.accept_connections(server_socket)
 
+    def make_request(self, query):
+        request = requests.get(query, headers=self.api_header)
+        return request.json()
 
-def port_to_code(port):
-    port = str(port)
-    res = ""
-    for number in port:
-        res += adict[number]
-    return res + "\n"
+    def send_data(self, connection):
+        for film in self.film_jsons:
+            connection.sendall(film.encode())
+
 
 
 if __name__ == '__main__':
